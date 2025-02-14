380da9192701886963c9ccd64c9c962552cdce21
prashant212345 <prashant.kumar_cs22@gla.ac.in>
2025-02-14 23:08:17 +0530

    Submission of Sorting Algorithms

---
+++ Sort_Id.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Sort_Id
+{
+	static void Main()
+	{
+		Console.Write("ENter Total Empolyees : ");
+		int n = int.Parse(Console.ReadLine());
+
+		Console.Write("\nEnter Total Ids for the Employee : ");
+		string[] inp = Console.ReadLine().Split(',');
+
+		int[] arr = new int[n];
+		for(int i = 0; i<n; i++)
+		{
+			arr[i] = int.Parse(inp[i]);
+		}
+		SortedId(arr);
+	}
+	public static void SortedId(int[] arr)
+	{
+		for (int i = 1; i < arr.Length; i++) { 
+			int key = arr[i];
+			int j = i-1;
+			while (j >= 0 && arr[j] > key)
+			{
+				arr[j+1] = arr[j];
+				j--;
+			}
+			arr[j+1] = key;
+		}
+		Console.Write("Sorted Ids : ");
+		foreach (int i in arr) { 
+			Console.Write(i + ",");
+		}
+    }
+}
---
+++ Sort_Marks.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Sort_Marks
+{
+    static void Main()
+    {
+        Console.Write("Enter total Students : ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("\nEnter the Marks for Students : ");
+        string[] inp = Console.ReadLine().Split(',');
+
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++)
+        {
+            arr[i] = int.Parse(inp[i]);
+        }
+
+        SortedMarks(arr);
+    }
+    public static void SortedMarks(int[] arr)
+    {
+        bool swapped;
+        for (int i = 0; i < arr.Length - 1; i++) { 
+            swapped = false;
+            for (int j = 0; j < arr.Length - i - 1; j++)
+            {
+                if(arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+        Console.Write("Sorted Marks of the Students : ");
+        foreach (int i in arr)
+        {
+            Console.Write(i + ",");
+        }
+    }
+}
